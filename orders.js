618e94afe0520dc97254daffff5b6e9b36c90e58
Bintang Syafrian Rizal <117698972+gnatnib@users.noreply.github.com>
2023-09-23 13:19:15 +0700

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+  {
+    productName: "Yamaha Mio 2012 - Oli Karbu Bocor",
+    productNumber: "00003",
+    paymentStatus: "Due",
+    status: "In Progress",
+  },
+  {
+    productName: "Yamaha Vixion 2015 - Ganti Oli",
+    productNumber: "00002",
+    paymentStatus: "Due",
+    status: "In Progress",
+  },
+  {
+    productName: "Yamaha Nmax 2018 - Lampu Mati",
+    productNumber: "00001",
+    paymentStatus: "Paid",
+    status: "Finished",
+  },
+];
